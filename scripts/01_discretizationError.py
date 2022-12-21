27feda281b87fe04404fbfbd5c820702c55a0326
Thibaut Lunet <thibaut.lunet@tuhh.de>
2022-12-21 19:40:41 +0100

    TL: improved discretizationError script

--- scripts/01_discretizationError.py
+++ scripts/01_discretizationError.py
@@ -13,14 +13,14 @@ from blockops import BlockProblem
 
 reLam = np.linspace(-4, 0.5, 500)
 imLam = np.linspace(-3, 3, 500)
-N = 10
-M = 4
+N = 1
+M = 5
 scheme = 'COLLOCATION'
 
 lam = reLam[:, None] + 1j*imLam[None, :]
 prob = BlockProblem(
     lam.ravel(), N, N, M, scheme,
-    nodes='CHEBY-3', qType='RADAU-RIGHT')
+    nodes='LEGENDRE', qType='LOBATTO')
 
 uExact = prob.getSolution('exact')
 uNum = prob.getSolution('fine')
@@ -28,20 +28,23 @@ err = np.abs(uExact-uNum)
 
 stab = np.abs(uNum)[0, :, -1].reshape(lam.shape)
 errEnd = err[-1, :, -1].reshape(lam.shape)
+errMax = np.max(err, axis=(0, -1)).reshape(lam.shape)
+
+err = errMax
 
 # Plot discretization error on complex plane
 coords = np.meshgrid(reLam.ravel(), imLam.ravel(), indexing='ij')
 
 plt.figure()
-errMin = -7
-errMax = 0
-levels = np.logspace(errMin, errMax, num=22)
-errEnd[errEnd < 10**errMin] = 10**errMin
-errEnd[errEnd > 10**errMax] = 10**errMax
-ticks = [10**(i) for i in range(errMin, errMax+1)]
-plt.contourf(*coords, errEnd, levels=levels, locator=ticker.LogLocator())
+eMin = -7
+eMax = 0
+levels = np.logspace(eMin, eMax, num=22)
+err[err < 10**eMin] = 10**eMin
+err[err > 10**eMax] = 10**eMax
+ticks = [10**(i) for i in range(eMin, eMax+1)]
+plt.contourf(*coords, err, levels=levels, locator=ticker.LogLocator())
 plt.colorbar(ticks=ticks, format=ticker.LogFormatter())
-CS = plt.contour(*coords, errEnd, levels=ticks,
+CS = plt.contour(*coords, err, levels=ticks,
                  colors='k', linestyles='--', linewidths=0.75)
 plt.contour(*coords, stab, levels=[1], colors='gray')
 plt.hlines(0, reLam.min(), reLam.max(), colors='black', linestyles='--')
