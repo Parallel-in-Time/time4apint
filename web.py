0db9c32ea747c4097561d360ea14da386cae633b
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-06-15 14:18:55 +0200

    TL: small modifications on website

--- web.py
+++ web.py
@@ -1,7 +1,8 @@
 from dynamic_site.site import Site
+import sys
 
 site = Site(
     apps_path='web_apps',
-    # enforce_dev_mode=True,
+    enforce_dev_mode=True if (len(sys.argv) > 1 and sys.argv[1] == "--dev") else False,
     escape_html_in_md=False)
 site.run()
